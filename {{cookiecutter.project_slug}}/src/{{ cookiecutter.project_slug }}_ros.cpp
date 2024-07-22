a59a96e47e84585bfe8c98cbe7ef7bf6caedf107
Ludwig <ludovico.scarton@gmail.com>
2024-07-22 13:03:29 +0200

    fixing testing

--- {{cookiecutter.project_slug}}/src/{{ cookiecutter.project_slug }}_ros.cpp
+++ {{cookiecutter.project_slug}}/src/{{ cookiecutter.project_slug }}_ros.cpp
@@ -1,13 +1,13 @@
 #include "{{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}_ros.h"
 #
-MyClassROS::MyClassROS(ros::NodeHandle& nh) : nh_(nh) {
+MyObjectROS::MyClassROS(ros::NodeHandle& nh) : nh_(nh) {
     // ROS-specific initialization
 }
 
-void MyClassROS::initialize() {
+void MyObjectROS::initialize() {
     // ROS-specific initialization code
 }
 
-void MyClassROS::update() {
+void MyObjectROS::update() {
     // ROS-specific update code
 }
